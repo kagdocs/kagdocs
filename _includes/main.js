db9193291a0eca224e0ccbce970d56fbf2eec5c2
AsuMagic <sdelang@sdelang.fr>
2017-07-04 21:52:05 +0200

    Fold most categories by default

--- _includes/main.js
+++ _includes/main.js
@@ -1,20 +1,31 @@
-var sidebar = document.getElementById('sidebar');
+var sidebar = document.getElementById('sidebar'),
+	categories = sidebar.querySelectorAll('h6');
+
+// Toggle folding for one category header
+function toggleFold(h6)
+{
+	var ul = h6.nextElementSibling;
+
+	if (ul.style.display == "none")
+	{
+		h6.classList.remove("folded");
+		ul.style.display = "block";
+	}
+	else
+	{
+		h6.classList.add("folded");
+		ul.style.display = "none";
+	}
+}
+
+// Toggle all category headers but the first one
+for (i = 1; i < categories.length; i++)
+{
+	toggleFold(categories[i]);
+}
 
 sidebar.addEventListener("click", function(event)
 {
 	if (event.target.nodeName == "H6")
-	{
-		var ul = event.target.nextElementSibling;
-
-		if (ul.style.display == "none")
-		{
-			event.target.classList.remove("folded");
-			ul.style.display = "block";
-		}
-		else
-		{
-			event.target.classList.add("folded");
-			ul.style.display = "none";
-		}
-	}
+		toggleFold(event.target);
 });
